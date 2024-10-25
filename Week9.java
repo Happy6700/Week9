fa25d2b3ff1db0ea4f2f57acbba0838e336c9c41
Happy <manncovey@gmail.com>
2024-10-25 11:21:37 -0600

    Week9
    I don't know if I did this right

---
+++ Week9.java
@@ -0,0 +1,14 @@
+/** Madison Covey
+ *  Softwear Programing class
+ *  Mr. Gross
+ */
+public class Week9 {
+public static void main(String[] args) {
+  int myNum1 = plusMethodInt (3, 6);
+  double myNum2 = plusMethodDouble (2.5, 2.50);
+System.out.println("int: " + myNum1);
+System.out.println("double: " + myNum2);
+}
+
+
+    
